aab2cb81ef0baeea4d1b5a9f5eb554c21a357eca
jruby -S gem install bundler <newbluelight1031@gmail.com>
2019-11-23 13:25:50 -0800

    set search button

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,13 @@ class Article < ApplicationRecord
 
   has_many :subcontents
   mount_uploader :image, ImageUploader
+
+  def self.search(search) #self.でクラスメソッドとしている
+   if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+     Article.where(['title LIKE ?', "%#{search}%"])
+   else
+     Article.all #全て表示。
+   end
+  end
+
 end
