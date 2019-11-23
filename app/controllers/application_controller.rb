804eb83ed8b81ac00d0b37301dc69cfa91250d81
jruby -S gem install bundler <newbluelight1031@gmail.com>
2019-11-23 13:39:48 -0800

    set up authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,24 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def admin_user
+    if user_signed_in? && current_user.admin == true
+
+    else
+      redirect_to root_path
+    end
+  end
+
+
   protected
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :admin])
   end
+
+
 
 
 end
--- app/controllers/subcontents_controller.rb
+++ app/controllers/subcontents_controller.rb
@@ -1,5 +1,7 @@
 class SubcontentsController < ApplicationController
   before_action :set_subcontent, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :admin_user
 
   # GET /subcontents
   # GET /subcontents.json
